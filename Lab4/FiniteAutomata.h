1dbf81314d5e4702ce215d4906cd22dcf7c5f98e
loco <loco@locos-Mac-mini.local>
2020-11-05 19:37:16 +0200

    Added Lab4

---
+++ Lab4/FiniteAutomata.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <vector>
+#include <string>
+using namespace std;
+struct TransitionFunction {
+    string s1;
+    string s2;
+    string s3;
+};
+
+class FiniteAutomata {
+    vector<string> states;
+    vector<string> alphabet;
+    vector<string> initialState;
+    vector<string> finalStates;
+    vector<TransitionFunction> transitionFunctions;
+public:
+    FiniteAutomata() {transitionFunctions.reserve(10);}
+    void readFromFile(char * fileName);
+    void printStates();
+    void printAlphabet();
+    void printInitialState();
+    void printFinalStates();
+    void printTransitionFunctions();
+};
