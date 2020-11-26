6ef3be0ed6d41bbc3fa376dbc72edd97e6034210
loco <loco@locos-Mac-mini.local>
2020-11-26 18:27:00 +0200

    Added lab5 - Same with Raul Rusu

---
+++ Lab5/LR0/Models/LR0Item.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace lab2.LR0.Models
+{
+    public enum SymbolType
+    {
+        Terminal,
+        Nonterminal,
+        Dot,
+        Unknown
+    }
+
+    public class LR0Item
+    {
+        public string lhs { get; set; }
+        public string rhs { get; set; }
+
+        private SymbolType GetSymbolType(char symbol)
+        {
+            if (symbol == '.')
+                return SymbolType.Dot;
+            if (Char.IsUpper(symbol))
+                return SymbolType.Nonterminal;
+            if (Char.IsLower(symbol))
+                return SymbolType.Terminal;
+
+            return SymbolType.Unknown;
+        }
+
+        private int GetDotIndex()
+        {
+            var index = 0;
+            foreach (var symbol in rhs)
+            {
+                if (GetSymbolType(symbol) == SymbolType.Dot)
+                    return index;
+                index++;
+            }
+
+            return -1;
+        }
+
+        public LR0Item(string lhs, string rhs)
+        {
+            this.lhs = lhs;
+            this.rhs = rhs;
+        }
+
+        public string GetNonterminalAfterDot()
+        {
+            var dotIndex = GetDotIndex();
+            if (dotIndex == rhs.Length - 1)
+                return null;
+
+            var symbol = rhs[dotIndex + 1];
+
+            if (GetSymbolType(symbol) != SymbolType.Nonterminal)
+                return null;
+
+            return symbol.ToString();
+        }
+
+        public string GetSymbolAfterDot()
+        {
+            var dotIndex = GetDotIndex();
+            if (dotIndex == rhs.Length - 1)
+                return null;
+
+            var symbol = rhs[dotIndex + 1];
+
+            return symbol.ToString();
+        }
+
+        public LR0Item MoveDot()
+        {
+            var dotIndex = GetDotIndex();
+
+            if (dotIndex == rhs.Length - 1)
+                return this;
+
+            var rhsBuilder = new StringBuilder(rhs);
+            rhsBuilder[dotIndex] = rhsBuilder[dotIndex + 1];
+            rhsBuilder[dotIndex + 1] = '.';
+
+            var newLR0Item = new LR0Item(lhs, rhsBuilder.ToString());
+            return newLR0Item;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is LR0Item lr0Item)
+            {
+                if (!lr0Item.lhs.Equals(lhs))
+                    return false;
+                if (!lr0Item.rhs.Equals(rhs))
+                    return false;
+            }
+            else
+                return false;
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return lhs + "->" + rhs;
+        }
+    }
+}
---
+++ Lab5/LR0/Models/LR0State.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2.LR0.Models
+{
+    public class LR0State
+    {
+        public LR0State(List<LR0Item> items)
+        {
+            Items = items;
+        }
+
+        public List<LR0Item> Items { get; set; }
+
+        public override string ToString()
+        {
+            var str = "";
+
+            Items.ForEach(item =>
+            {
+                str += item.ToString() + "\n";
+            });
+
+            return str;
+        }
+    }
+}
